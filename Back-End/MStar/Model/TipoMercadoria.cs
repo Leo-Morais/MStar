99669f50e8a56446a0d5b4cc1e75cffb629edf7d
Leo-Morais <leo.vgvm@gmail.com>
2024-10-03 23:32:42 -0300

    Adicionado Modelo TipoMercadoria

---
+++ Back-End/MStar/Model/TipoMercadoria.cs
@@ -0,0 +1,15 @@
+﻿namespace MStar.Model
+{
+    public class TipoMercadoria
+    {
+        public TipoMercadoria() { }
+
+        public TipoMercadoria(string tipo) 
+        { 
+            this.Tipo = tipo;
+        }
+
+        public int Id { get; set; }
+        public string Tipo { get; set; }
+    }
+}
