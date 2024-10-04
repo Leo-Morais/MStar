3180ff8e55bbf74e26d68f9807d5b12dd8bb1dd4
Leo-Morais <leo.vgvm@gmail.com>
2024-10-04 15:06:39 -0300

    Adicionado a relação da tabela movimentação

--- Back-End/MStar/Repository/ConnectionContext.cs
+++ Back-End/MStar/Repository/ConnectionContext.cs
@@ -12,6 +12,7 @@ namespace MStar.Repository
 
         public DbSet<Mercadoria> Mercadoria { get; set; }
         public DbSet<TipoMercadoria> TipoMercadoria { get; set; }
+        public DbSet<Movimentacao> Movimentacao { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -30,14 +31,24 @@ namespace MStar.Repository
             modelBuilder.Entity<Mercadoria>().Property(x => x.Nome).HasColumnName("Nome");
             modelBuilder.Entity<Mercadoria>().Property(x => x.DataCriacao).HasColumnName("DataCriacao");
             modelBuilder.Entity<Mercadoria>().Property(x => x.TipoMercadoriaId).HasColumnName("TipoMercadoriaId");
-            //modelBuilder.Entity<Mercadoria>()
-            //   .HasOne(x => x.TipoMercadoria)
-            //   .WithOne()
-            //   .HasForeignKey<TipoMercadoria>(e => e.Id)
-            //   .HasPrincipalKey<Mercadoria>(c => c.TipoMercadoriaId)
-            //   .OnDelete(DeleteBehavior.ClientNoAction);
-            modelBuilder.Entity<Mercadoria>().HasOne(x => x.TipoMercadoria).WithOne().HasForeignKey<TipoMercadoria>(x => x.Id).HasPrincipalKey<Mercadoria>(c => c.TipoMercadoriaId).OnDelete(DeleteBehavior.ClientNoAction);
+            modelBuilder.Entity<Mercadoria>().HasOne(x => x.TipoMercadoria).WithOne()
+                .HasForeignKey<TipoMercadoria>(x => x.Id).HasPrincipalKey<Mercadoria>(c => c.TipoMercadoriaId)
+                .OnDelete(DeleteBehavior.ClientNoAction);
             modelBuilder.Entity<Mercadoria>().Navigation(x => x.TipoMercadoria).AutoInclude();
+
+            //Mapeamento de Movimentação
+            modelBuilder.Entity<Movimentacao>().HasKey(x => x.Id);
+            modelBuilder.Entity<Movimentacao>().Property(e => e.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<Movimentacao>().ToTable("Movimentacao");
+            modelBuilder.Entity<Movimentacao>().Property(x => x.Quantidade).HasColumnName("Quantidade");
+            modelBuilder.Entity<Movimentacao>().Property(x => x.LocalMovimentacao).HasColumnName("LocalMovimentacao");
+            modelBuilder.Entity<Movimentacao>().Property(x => x.TipoMovimentacao).HasColumnName("TipoMovimentacao");
+            modelBuilder.Entity<Movimentacao>().Property(x => x.DataCriacao).HasColumnName("DataCriacao");
+            modelBuilder.Entity<Movimentacao>().Property(x => x.IdMercadoria).HasColumnName("IdMercadoria");
+            modelBuilder.Entity<Movimentacao>().HasOne(x => x.Mercadoria).WithOne()
+                .HasForeignKey<Mercadoria>(x => x.Id).HasPrincipalKey<Movimentacao>(c => c.IdMercadoria)
+                .OnDelete(DeleteBehavior.ClientNoAction);
+            modelBuilder.Entity<Movimentacao>().Navigation(x => x.Mercadoria).AutoInclude();
         }
     }
 }
